0734199e44014549c99f19d7f8f3cb05affee829
dgw <30551889+Uadj@users.noreply.github.com>
2022-01-04 00:55:19 +0900

    Initial commit

---
+++ src/test/java/com/example/demo5/service/PostsService.java
@@ -0,0 +1,18 @@
+package com.example.demo5.service;
+
+import com.example.demo5.posts.PostsRepository;
+import com.example.demo5.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
