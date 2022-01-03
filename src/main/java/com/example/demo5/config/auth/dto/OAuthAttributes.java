bf0d24d658fcbab2d326d009cd0d5118efeb2a80
dgw <30551889+Uadj@users.noreply.github.com>
2022-01-04 06:06:55 +0900

    Update OAuthAttributes.java

--- src/main/java/com/example/demo5/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/example/demo5/config/auth/dto/OAuthAttributes.java
@@ -23,8 +23,8 @@ public class OAuthAttributes {
         this.email = email;
         this.picture = picture;
     }
-    public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes){
-        if("naver".equals(registrationId)){
+    public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if("naver".equals(registrationId)) {
             return ofNaver("id", attributes);
         }
         return ofGoogle(userNameAttributeName, attributes);
@@ -38,12 +38,13 @@ public class OAuthAttributes {
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
-    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes){
-        Map<String, Object> response = (Map<String, Object>)attributes.get("response");
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
         return OAuthAttributes.builder()
-                .name((String) attributes.get("name"))
-                .email((String) attributes.get("email"))
-                .picture((String) attributes.get("profile_image"))
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
                 .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
