3db0600348a2cf98aef86ee3cdaefbb176d2910c
dgw <30551889+Uadj@users.noreply.github.com>
2022-01-04 03:23:27 +0900

    google login func finished

--- src/main/java/com/example/demo5/web/IndexController.java
+++ src/main/java/com/example/demo5/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.demo5.web;
 
+import com.example.demo5.config.auth.LoginUser;
 import com.example.demo5.config.auth.dto.SessionUser;
 import com.example.demo5.service.PostsService;
 import com.example.demo5.web.dto.PostsResponseDto;
@@ -19,9 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user!=null){
             model.addAttribute("userName", user.getName());
         }
